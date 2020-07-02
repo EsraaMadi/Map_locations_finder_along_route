20232d3b2548ef1731cf2638c50380b5963e1690
EsraaMadi <esraa.sabry.mohamed@gmail.com>
2020-07-02 22:42:52 +0300

    fix installation error

--- create_config.sh
+++ create_config.sh
@@ -1,5 +1,13 @@
-mkdir -p ~/.streamlit 
-echo "[server]
-headless = true
-port = $PORTenableCORS = false
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"esraa.sabry.mohamed@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
