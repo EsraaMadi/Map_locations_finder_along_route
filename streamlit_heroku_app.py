351f66f3a5069df3a5f22faf2b7d47ef72bda109
EsraaMadi <esraa.sabry.mohamed@gmail.com>
2020-07-05 17:09:15 +0300

    fix map display

--- streamlit_heroku_app.py
+++ streamlit_heroku_app.py
@@ -398,6 +398,8 @@ def main():
     progress_bar.progress(.8)
     #map_box = st.markdown(map_._repr_html_(), unsafe_allow_html=True)
     #map_box = st.write(map_._repr_html_(), unsafe_allow_html=True)
+    map_.save("test.html")
+    st.markdown('<iframe src="/test.html"> </iframe>')
 
     # remove progress bar
     weights_warning.empty()
